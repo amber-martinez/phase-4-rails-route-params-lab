c3f18e4db57492d3493bb8253ee0c74f243d6124
Amber <martinez.amber1028@gmail.com>
2022-11-14 13:58:05 -0800

    commit tree a65aac13bf062d8f18028457724435637e771c77

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,14 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def firstname
+    byebug
+    # student = Student.find(params[:first_name])
+  end
+
 end
